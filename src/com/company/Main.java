65d0491211db9ff3de7a8862d7649bf4732f5563
Demian Foliush <Demian.Foliush.IR.2021@lpnu.ua>
2022-03-27 18:43:56 +0300

    add laba2 code

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pharmacy arta = new Pharmacy();
+        MedicineBox Validol = new MedicineBox();
+        Medicine validol = new Medicine();
+
+
+    }
+}
