cb2912d2257efccd4ad1503352d6f945741d04a8
Demian Foliush <Demian.Foliush.IR.2021@lpnu.ua>
2022-05-09 22:22:11 +0300

    fixed convention code

--- src/main/java/com/company/manager/TextEditor.java
+++ src/main/java/com/company/manager/TextEditor.java
@@ -3,10 +3,7 @@ package com.company.manager;
 import lombok.AllArgsConstructor;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.Scanner;
-import java.util.Set;
 
 
 @AllArgsConstructor
@@ -31,7 +28,7 @@ public class TextEditor {
         return text;
     }
 
-    public String substituteNumbersForCorrespondingLetters(){
+    public String substituteNumbersForCorrespondingLetters() {
         String text = getTextFromFile();
         assert text != null;
         String result = text.replaceAll("(?<!\\S)1(?![^\\s.,?!])", "A")
@@ -43,7 +40,6 @@ public class TextEditor {
                 .replaceAll("(?<!\\S)7(?![^\\s.,?!])", "G")
                 .replaceAll("(?<!\\S)8(?![^\\s.,?!])", "H")
                 .replaceAll("(?<!\\S)9(?![^\\s.,?!])", "I");
-        System.out.println(result);
         return result;
     }
 }
