eb3db3e8cf94252e1a85d19ae39af8c1e62518b4
Demian Foliush <Demian.Foliush.IR.2021@lpnu.ua>
2022-05-13 15:59:26 +0300

    added enhanced functionality after a friendly review

--- src/main/java/com/company/manager/TextEditor.java
+++ src/main/java/com/company/manager/TextEditor.java
@@ -31,16 +31,24 @@ public class TextEditor {
     public String substituteNumbersForCorrespondingLetters() {
         String text = getTextFromFile();
         assert text != null;
-        String result = text.replaceAll("(?<!\\S)1(?![^\\s.,?!])", "A")
-                .replaceAll("(?<!\\S)2(?![^\\s.,?!])", "B")
-                .replaceAll("(?<!\\S)3(?![^\\s.,?!])", "C")
-                .replaceAll("(?<!\\S)4(?![^\\s.,?!])", "D")
-                .replaceAll("(?<!\\S)5(?![^\\s.,?!])", "E")
-                .replaceAll("(?<!\\S)6(?![^\\s.,?!])", "F")
-                .replaceAll("(?<!\\S)7(?![^\\s.,?!])", "G")
-                .replaceAll("(?<!\\S)8(?![^\\s.,?!])", "H")
-                .replaceAll("(?<!\\S)9(?![^\\s.,?!])", "I");
+        String result = text.replaceAll("(?<!\\S)1(?![^\\s.?!])", "A")
+                .replaceAll("(?<!\\S)1,\\s", "A, ")
+                .replaceAll("(?<!\\S)2(?![^\\s.?!])", "B")
+                .replaceAll("(?<!\\S)2,\\s", "B, ")
+                .replaceAll("(?<!\\S)3(?![^\\s.?!])", "C")
+                .replaceAll("(?<!\\S)3,\\s", "C, ")
+                .replaceAll("(?<!\\S)4(?![^\\s.?!])", "D")
+                .replaceAll("(?<!\\S)4,\\s", "D, ")
+                .replaceAll("(?<!\\S)5(?![^\\s.?!])", "E")
+                .replaceAll("(?<!\\S)5,\\s", "E, ")
+                .replaceAll("(?<!\\S)6(?![^\\s.?!])", "F")
+                .replaceAll("(?<!\\S)6,\\s", "F, ")
+                .replaceAll("(?<!\\S)7(?![^\\s.?!])", "G")
+                .replaceAll("(?<!\\S)7,\\s", "G, ")
+                .replaceAll("(?<!\\S)8(?![^\\s.?!])", "H")
+                .replaceAll("(?<!\\S)8,\\s", "H, ")
+                .replaceAll("(?<!\\S)9(?![^\\s.?!])", "I")
+                .replaceAll("(?<!\\S)9,\\s", "I, ");
         return result;
     }
 }
-
--- src/test/java/com/company/manager/TextEditorTest.java
+++ src/test/java/com/company/manager/TextEditorTest.java
@@ -10,14 +10,14 @@ public class TextEditorTest {
     void testSubstituteNumbersForCorrespondingLetters() {
         TextEditor text = new TextEditor("src/main/resources/randomInputOfText.txt");
 
-        StringBuilder sb = new StringBuilder("A 123451 A 765 A F E A.\n");
-        sb.append("B 123451 B 765 B F E B.\n");
+        StringBuilder sb = new StringBuilder("A, 123451 A 765 1,123 F E A.\n");
+        sb.append("B 123451 B. 765 B F E B.\n");
         sb.append("C 123451 C 765 C F E C.\n");
         sb.append("D 123451 D 765 D F E D.\n");
         sb.append("E 123451 E 765 E F E E.\n");
         sb.append("F 123451 F 765 F F E F.\n");
-        sb.append("G 123451 G 765 G F E G,\n");
-        sb.append("H 123451 H 765 H F E H?\n");
+        sb.append("G 123451 G 765 G F E G.\n");
+        sb.append("H, 123451. H 765 H H, H, H. 8,8 E H?\n");
         sb.append("I 123451 I 765 I F E I!\n");
         String expectedOutputOfResult = sb.toString();
 
@@ -25,3 +25,11 @@ public class TextEditorTest {
         assertEquals(expectedOutputOfResult, actualOutputOfResult);
     }
 }
+
+
+
+
+
+
+
+
